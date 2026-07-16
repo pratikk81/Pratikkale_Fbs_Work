b2fb04dd978d93d129611202ae5fb77d22f646b5
pratikk81 <pratik2020kk@gmail.com>
2026-07-16 16:17:17 +0530

    Add files via upload

---
+++ Core Python/Demo/basice/Branching/nested_if_else_demo.py
@@ -0,0 +1,16 @@
+gender = input('Enter gender(m/f) :')
+age = int(input('Enter age'))
+
+if(gender == 'm'):
+    if(age >= 21):
+     print('boy is eligibal for marrage')
+    else:
+       print('pehale padhai kar lo')
+
+else:
+   if(age >= 18):
+      print('girl is eligibal for marrage')
+   else:
+      print('pahale padhai kar lo')
+  
+        
