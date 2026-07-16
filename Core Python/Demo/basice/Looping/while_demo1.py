97bbda50a4ebd566f9eae055d5d919c0e4e8ebc7
pratikk81 <pratik2020kk@gmail.com>
2026-07-16 16:07:30 +0530

    Add files via upload

---
+++ Core Python/Demo/basice/Looping/while_demo1.py
@@ -0,0 +1,14 @@
+#syntex:
+#while(condition):
+   #block of code
+
+
+i = 1                           #variable initilization
+while(i <= 10 ):                  #variable used in condition
+    print('Hello   !')
+    i += 1    #i = i+ 1  #variable value change
+
+   
+
+
+
