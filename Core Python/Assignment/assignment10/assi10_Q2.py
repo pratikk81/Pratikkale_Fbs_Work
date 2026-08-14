af012cef636e472a1e5e441f43e84f0580bf2ee1
PRATIK KISHOR KALE <pratik2020kk@gmail.com>
2026-08-14 10:26:05 +0530

    Add files via upload

---
+++ Core Python/Assignment/assignment10/assi10_Q2.py
@@ -0,0 +1,15 @@
+li=[10,20,30,40,50,60,70]
+max = li[0]
+min = li[0]
+
+for i in li:
+    if i > max:
+        max = i
+    if i < min:
+        min = i
+
+print('list :',li)
+
+print('maximum number of list is :',max)
+print('minimum number of list is :',min)
+        
